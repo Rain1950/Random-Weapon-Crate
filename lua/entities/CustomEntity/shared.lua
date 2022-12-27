6848edeaa40c643911296097c97498d440ce55c1
Rain <maxeter@onet.pl>
2022-12-28 00:31:00 +0100

    Naming changes (shared.lua)
    Changed placeholder names to normal ones

--- lua/entities/CustomEntity/shared.lua
+++ lua/entities/CustomEntity/shared.lua
@@ -1,6 +1,6 @@
 ENT.Author = "Rain"
 ENT.Base = "base_gmodentity"
-ENT.PrintName = "My Entity"
-ENT.Instructions = "Floppa"
+ENT.PrintName = "Random Weapon Box"
+ENT.Instructions = "Use it to acquire  random weapon"
 ENT.Spawnable = true
 ENT.Type = "anim"
